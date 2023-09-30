a0f551384900aea6dcba032508f3f20d6de54f04
ShubhamSawant532 <shubhamravsawant532002@gmail.com>
2023-09-30 20:11:15 +0530

    implement pir sensor and add notification to report the motion status

--- src/pir_sensor.cpp
+++ src/pir_sensor.cpp
@@ -30,3 +30,46 @@
 #include <Arduino.h>
 #include <board.h>
 
+namespace PIRSensor
+{
+    static uint8_t  f_last_pin_state = LOW;
+    static enum State f_state = PIRSensor::PRESENCE_NOT_DETECTED;
+    static cb_notify f_cb = NULL;
+    static void *f_context = NULL;
+
+    void setup(cb_notify i_cb, void *i_context)
+    {
+        pinMode(PIR_SENSOR_PIN, INPUT_PULLUP); // set arduino pin to input pull-up mode
+        f_cb = i_cb;
+        f_context = i_context;
+    }
+
+    void loop(void)
+    {
+        static uint8_t current_pin_state;
+        current_pin_state  = digitalRead(PIR_SENSOR_PIN); // read new state
+
+        if ((f_last_pin_state == LOW) && (current_pin_state == HIGH))  // state change: LOW -> HIGH
+        {
+            f_state = PIRSensor::PRESENCE_DETECTED;
+            if(f_cb) f_cb(f_context, f_state);
+            Serial.println("Presense detected");
+        }
+        else if ((f_last_pin_state == HIGH) && (current_pin_state == LOW))  // state change: HIGH -> LOW
+        {
+            f_state = PIRSensor::PRESENCE_NOT_DETECTED;
+            if(f_cb) f_cb(f_context, f_state);
+            Serial.println("motion not detected");
+        }
+        else
+        {
+        }
+        f_last_pin_state = current_pin_state;              // save the last state
+    }
+
+    PIRSensor::State getStatus(void)
+    {
+        return f_state;
+    }
+}
+
